841e9aefaf5dea12179a084b79064c994f16ed3a
Trackt-ion <65196513+Trackt-ion@users.noreply.github.com>
2020-11-23 14:20:31 -0800

    this is to test if it reads scripts

---
+++ PRJCT X/Assets/testscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     /// test 
+        // to see if it does the thing 
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
